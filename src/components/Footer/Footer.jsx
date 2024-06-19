4c92c3d9742a67caa463d3f4e2e6f9931b786856
Nitish Kumar Samal <nitishskumar151@gmail.com>
2024-06-19 15:36:03 +0530

    fix3 footer background

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,18 @@
 // import GreenForest from "../../assets/images/footer-images/greenish-jungle.png";
 import NewsLetter from "./Newsletter/Newsletter";
 import MainFooter from "./MainFooter/MainFooter";
-import "./MainFooter/MainFooter.css";
+// import "./MainFooter/MainFooter.css";
 import BottomFooter from "./MainFooter/BottomFooter";
 
-function Footer() {
-  return (
-    <section className="Footer-Section bg-footer-green-jungle bg-no-repeat bg-cover text-white flex flex-col gap-2 w-full py-4 pt-10">
-      <NewsLetter />
-      <div className="MainFooter mt-8 px-2">
-        <MainFooter />
-      </div>
-      <hr className="max-w-7xl mx-auto rounded w-full" />
-      <BottomFooter />
-    </section>
-  );
-}
+const Footer = () => (
+  <section className="Footer-Section bg-footer-green-jungle bg-no-repeat bg-cover text-white flex flex-col gap-2 w-full py-4 pt-10">
+    <NewsLetter />
+    <div className="MainFooter mt-8 px-2">
+      <MainFooter />
+    </div>
+    <hr className="max-w-7xl mx-auto rounded w-full" />
+    <BottomFooter />
+  </section>
+);
 
 export default Footer;
