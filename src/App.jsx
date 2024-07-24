c6120b019f134983b5aa1c18f5327a80570df627
Perseusx99007 <ratulsaha99@gmail.com>
2024-07-24 15:35:38 +0530

    Culture details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import ContactPage from './Components/ContactPage'
+// import ContactPage from './Components/ContactPage'
+import CultureDetails from './Components/CultureDetails'
 //import './App.css'
 
 function App() {
   
 
   return (
-    <ContactPage/>
+    // <ContactPage/>
+    <CultureDetails/>
   )
 }
 
