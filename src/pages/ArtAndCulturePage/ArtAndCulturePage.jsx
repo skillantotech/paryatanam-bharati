992e36f58f9175a16cc5112e26e6cecb0bb61f0b
Nitish Kumar Samal <nitishskumar151@gmail.com>
2024-08-02 01:04:12 +0530

    fix scroll behavior of ArtAndCulture page

--- src/pages/ArtAndCulturePage/ArtAndCulturePage.jsx
+++ src/pages/ArtAndCulturePage/ArtAndCulturePage.jsx
@@ -1,4 +1,4 @@
-// import ArtandCulture from "../../components/ArtandCulturePage/ArtandCulture/ArtandCulture";
+import { useEffect } from "react";
 import Breadcrumbs from "../../components/ArtandCulturePage/BreadCrumb/Breadcrumbs";
 import HeroSection from "../../components/ArtandCulturePage/HeroSection/HeroSection";
 import ArtCultureSection from "../../components/ArtandCulturePage/ArtCultureSection/ArtCultureSection";
@@ -6,6 +6,10 @@ import Overview from "../../components/ArtandCulturePage/Overview/Overview";
 import Popularartcard from "../../components/ArtandCulturePage/Popularartcard/Popularartcard";
 import ClrOfCulture from "../../components/ArtandCulturePage/ClrOfCulture/ClrOfCulture";
 const ArtAndCulture = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <HeroSection />
