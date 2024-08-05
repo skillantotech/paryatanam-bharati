2cca09a4e4d704b15639146dc433ba0ddd6c8410
Perseusx99007 <ratulsaha99@gmail.com>
2024-08-05 12:44:58 +0530

    MINOR BUG FIXES

--- src/Components/CultureDetails.jsx
+++ src/Components/CultureDetails.jsx
@@ -12,36 +12,41 @@ const CultureDetails = () => {
   return (
     <section className = "max-w-7xl mx-auto px-4 flex lg:flex-row flex-col gap-10 my-20 overflow-hidden">
       <div className="max-w-full">
-        <div className="bg-red-500 text-white py-0.5 px-0.5 rounded-lg text-center max-w-12 text-xs">Dance</div>
-          <h2 className="font-bold text-7xl">Odissi</h2>
-            <div className="my-10 bg-slate-400 w-full flex justify-center rounded-lg overflow-hidden ">
-              <img className="aspect-[19/10] w-full object-cover" src={odissi}/> 
+        <h2 className="font-bold lg:text-4xl text-3xl">Odissi</h2>
+        <div className="bg-blue-600 text-white py-0.5 px-0.5 rounded-lg text-center max-w-12 text-xs mt-2">Dance</div>
+        <div className="overView">
+          <div className="my-10 bg-slate-400 w-full flex justify-center rounded-lg overflow-hidden ">
+            <img className="aspect-[19/10] w-full object-cover" src={odissi}/> 
+          </div>
+          <div className="flex flex-col gap-3">
+          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, nesciunt vitae libero, eveniet  numquam perspiciatis ipsam impedit nam debitis odit labore ipsum similique velit? Eos animi voluptates aliquam delectus!</p>
+            <p>Tenetur voluptatum reiciendis inventore esse sequi at corporis iusto? Rerum neque ipsa voluptates iure, consequatur incidunt atque perferendis molestiae sed nobis omnis perspiciatis, quae non expedita obcaecati tempora molestias repellat.</p>
+          </div>
+          <div className="imageContainer grid grid-cols-2 md:grid-cols-2 gap-4 my-10 md:h-[600px]">
+            <div className=" row-span-2 col-span-2 md:col-span-1 relative overflow-hidden rounded-lg">
+              <img className="w-full h-full object-cover" src={odissi}/>
             </div>
-            <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, nesciunt vitae libero, eveniet  numquam perspiciatis ipsam impedit nam debitis odit labore ipsum similique velit? Eos animi voluptates aliquam delectus!
-              <br></br>
-              <br></br>
-            Tenetur voluptatum reiciendis inventore esse sequi at corporis iusto? Rerum neque ipsa voluptates iure, consequatur incidunt atque perferendis molestiae sed nobis omnis perspiciatis, quae non expedita obcaecati tempora molestias repellat.</p>
-            <div className="grid grid-cols-2 md:grid-cols-2 gap-4 my-10 md:h-[600px]">
-                <div className=" row-span-2 col-span-2 md:col-span-1 relative overflow-hidden rounded-lg">
-                  <img className="w-full h-full object-cover" src={odissi}/>
-                </div>
 
-                <div className="relative overflow-hidden rounded-lg">
-                  <img className="w-full h-full object-cover" src={odissi}/>
-                </div>
-
-                <div className="relative overflow-hidden rounded-lg md:col-auto">
-                  <img className="w-full h-full object-cover" src={odissi}/>
-                </div>
+            <div className="relative overflow-hidden rounded-lg">
+              <img className="w-full h-full object-cover" src={odissi}/>
             </div>
-            <h3 className="font-medium text-3xl my-10">Significance</h3>
-            <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, nesciunt vitae libero, eveniet  numquam perspiciatis ipsam impedit nam debitis odit labore ipsum similique velit? Eos animi voluptates aliquam delectus!
-              <br></br>
-              <br></br>
-            Tenetur voluptatum reiciendis inventore esse sequi at corporis iusto? Rerum neque ipsa voluptates iure, consequatur incidunt atque perferendis molestiae sed nobis omnis perspiciatis, quae non expedita obcaecati tempora molestias repellat.</p>
-      </div>   
+
+            <div className="relative overflow-hidden rounded-lg md:col-auto">
+              <img className="w-full h-full object-cover" src={odissi}/>
+            </div>
+          </div>
+          </div>
+          <div className="secondParagraph">
+            <h3 className="font-medium text-3xl mt-10 mb-5">Significance</h3>
+            <div className="flex flex-col gap-3">
+              <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, nesciunt vitae libero, eveniet  numquam perspiciatis ipsam impedit nam debitis odit labore ipsum similique velit? Eos animi voluptates aliquam delectus!
+              </p>
+              <p>Tenetur voluptatum reiciendis inventore esse sequi at corporis iusto? Rerum neque ipsa voluptates iure, consequatur incidunt atque perferendis molestiae sed nobis omnis perspiciatis, quae non expedita obcaecati tempora molestias repellat.</p>
+              </div>
+            </div> 
+            </div>  
       <div className="lg:min-w-96 max-w-full overflow-hidden">
-        <h3 className="mt-12 font-medium text-4xl flex justify-center">Categories</h3>
+        <h3 className="mt-7 font-medium text-3xl flex justify-center">Categories</h3>
         <div className="my-11 flex flex-col items-center gap-7 overflow-hidden">
           <div className="relative group rounded-lg overflow-hidden max-w-80 h-28 ease-in-out duration-1000">
             <div className="flex items-center justify-center bg-black/20 group-hover:bg-black/60 w-full h-full z-50 absolute ease-in-out duration-1000 text-white text-center text-2xl font-semibold">Bharatanatyam</div>
